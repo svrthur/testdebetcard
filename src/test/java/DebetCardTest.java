41ec42507704793dde690c6b52fe962fa462e5fb
arthur.salahutdinov <artes_93@mail.ru>
2024-10-30 18:44:00 +0300

    бновлены build.gradle и DebetCardTest.java

--- src/test/java/DebetCardTest.java
+++ src/test/java/DebetCardTest.java
@@ -10,7 +10,7 @@ public class DebetCardTest {
     @Test
     void shouldTestSuccess() {
         open("http://localhost:9999");
-        $("[data-test-id=name] input").setValue("Дмитрий Логинов - Зернов");
+        $("[data-test-id=name] input").setValue("Артур Салахутдинов");
         $("[data-test-id=phone] input").setValue("+79290234567");
         $("[data-test-id=agreement]").click();
         $(".button__content").click();
@@ -20,7 +20,7 @@ public class DebetCardTest {
     @Test
     void shouldTestNameEnglishLetters() {
         open("http://localhost:9999");
-        $("[data-test-id=name] input").setValue("Dmitry");
+        $("[data-test-id=name] input").setValue("Arthur");
         $("[data-test-id=phone] input").setValue("+79290234567");
         $("[data-test-id=agreement]").click();
         $(".button__content").click();
@@ -30,8 +30,8 @@ public class DebetCardTest {
     @Test
     void shouldTestNameNumbers() {
         open("http://localhost:9999");
-        $("[data-test-id=name] input").setValue("Дмитри3");
-        $("[data-test-id=phone] input").setValue("+79290134567");
+        $("[data-test-id=name] input").setValue("Артур3");
+        $("[data-test-id=phone] input").setValue("+79290234567");
         $("[data-test-id=agreement]").click();
         $(".button__content").click();
         $("[data-test-id=name] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
@@ -39,8 +39,8 @@ public class DebetCardTest {
     @Test
     void shouldTestNameSymbols() {
         open("http://localhost:9999");
-        $("[data-test-id=name] input").setValue("=Дмитрий");
-        $("[data-test-id=phone] input").setValue("+79291234567");
+        $("[data-test-id=name] input").setValue("=Артур");
+        $("[data-test-id=phone] input").setValue("+79290234567");
         $("[data-test-id=agreement]").click();
         $(".button__content").click();
         $("[data-test-id=name] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
@@ -48,7 +48,7 @@ public class DebetCardTest {
     @Test
     void shouldTestPhoneSymbols() {
         open("http://localhost:9999");
-        $("[data-test-id=name] input").setValue("Дмитрий");
+        $("[data-test-id=name] input").setValue("Артур");
         $("[data-test-id=phone] input").setValue("++7929023456");
         $("[data-test-id=agreement]").click();
         $(".button__content").click();
@@ -57,7 +57,7 @@ public class DebetCardTest {
     @Test
     void shouldTestPhoneSpace() {
         open("http://localhost:9999");
-        $("[data-test-id=name] input").setValue("Дмитрий");
+        $("[data-test-id=name] input").setValue("Артур");
         $("[data-test-id=phone] input").setValue("+7929 23456");
         $("[data-test-id=agreement]").click();
         $(".button__content").click();
@@ -66,7 +66,7 @@ public class DebetCardTest {
     @Test
     void shouldTestPhoneLettersOInsteadOf0() {
         open("http://localhost:9999");
-        $("[data-test-id=name] input").setValue("Дмитрий");
+        $("[data-test-id=name] input").setValue("Артур");
         $("[data-test-id=phone] input").setValue("+7929O23456");
         $("[data-test-id=agreement]").click();
         $(".button__content").click();
@@ -75,7 +75,7 @@ public class DebetCardTest {
     @Test
     void shouldTestPhone10Digits() {
         open("http://localhost:9999");
-        $("[data-test-id=name] input").setValue("Дмитрий");
+        $("[data-test-id=name] input").setValue("Артур");
         $("[data-test-id=phone] input").setValue("+7929O23456");
         $("[data-test-id=agreement]").click();
         $(".button__content").click();
@@ -84,10 +84,11 @@ public class DebetCardTest {
     @Test
     void shouldTestPhoneSymbolBeforeNumber() {
         open("http://localhost:9999");
-        $("[data-test-id=name] input").setValue("Дмитрий");
+        $("[data-test-id=name] input").setValue("АРтур");
         $("[data-test-id=phone] input").setValue("=79290234567");
         $("[data-test-id=agreement]").click();
         $(".button__content").click();
         $("[data-test-id=phone] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 }
+
